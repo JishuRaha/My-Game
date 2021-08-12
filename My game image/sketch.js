51241a5f8f5f5384ef2d93c1c26c257d6b8b6877
JishuRaha <74971435+JishuRaha@users.noreply.github.com>
2021-08-12 12:29:52 +0530

    Add files via upload

---
+++ My game image/sketch.js
@@ -0,0 +1,19 @@
+var man
+
+function preload() {
+
+}
+
+function setup() {
+man=createSprite(200,200,40,40);
+
+}
+
+function draw() {
+    createCanvas(400,400)
+    backGround(0);
+    drawSprites();
+
+}
+
+
